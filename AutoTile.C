21ea48f2cb265ee0698e7cb8366a88f2f824d25f
Eric Liu <eric_liuu@outlook.com>
2022-04-10 22:07:39 -0400

    clean-up generator pass, remove mistakenly commited binary

--- AutoTile.C
+++ AutoTile.C
@@ -14,65 +14,71 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
   // Build a project
-  SgProject *project = frontend (argc,argv);
-  ROSE_ASSERT (project != NULL);
+  SgProject *project = frontend(argc,argv);
+  ROSE_ASSERT(project != NULL);
 
   // For each source file in the project
   SgFilePtrList & ptr_list = project->get_fileList();
+  int testcaseNum = 0;
 
-  for (SgFilePtrList::iterator iter = ptr_list.begin(); 
-               iter!=ptr_list.end(); iter++) { 
-    SgFile* sageFile = (*iter); 
-    SgSourceFile * sfile = isSgSourceFile(sageFile);
+  for (SgFilePtrList::iterator iter = ptr_list.begin(); iter != ptr_list.end();
+       iter++) {
+
+    SgFile *sageFile = (*iter);
+    SgSourceFile *sfile = isSgSourceFile(sageFile);
     ROSE_ASSERT(sfile);
     SgGlobal *root = sfile->get_globalScope();
-    SgDeclarationStatementPtrList& declList = root->get_declarations ();
+    SgDeclarationStatementPtrList& declList = root->get_declarations();
+    string fileName = sfile->getFileName();
 
-    cout << "Found a file: " << sfile->getFileName() << endl;
+    cout << "Found a file: " << fileName << endl;
 
-    //For each function body in the scope
-    for (SgDeclarationStatementPtrList::iterator p = declList.begin(); 
-            p != declList.end(); ++p) {
+    // For each function body in the scope
+    for (SgDeclarationStatementPtrList::iterator p = declList.begin();
+         p != declList.end(); ++p) {
+
       SgFunctionDeclaration *func = isSgFunctionDeclaration(*p);
       if (func == 0)  continue;
       SgFunctionDefinition *defn = func->get_definition();
       if (defn == 0)  continue;
-       //ignore functions in system headers, Can keep them to test robustness
-      if (defn->get_file_info()->get_filename()!=sageFile->get_file_info()->get_filename())
+      // Ignore functions in system headers, Can keep them to test robustness
+      if (defn->get_file_info()->get_filename()
+          != sageFile->get_file_info()->get_filename()) {
         continue;
+      }
 
       cout << "\t Found a function" << endl;
 
-      // Collect all analyable loops in this function
-      set<SgForStatement*> analyzableLoops;
+      // Collect all loops
+      Rose_STL_Container<SgNode*> loops = NodeQuery::querySubTree(
+          defn,V_SgForStatement);
+      if (loops.size() == 0) continue;
 
-      // For each loop 
-      Rose_STL_Container<SgNode*> loops = NodeQuery::querySubTree(defn,V_SgForStatement); 
-      if (loops.size()==0) continue;
+      // For each loop, collect loop features:
+      // - invariant, prefetched, non-prefetched references
+      for (Rose_STL_Container<SgNode*>::iterator iter = loops.begin();
+           iter != loops.end(); iter++) {
 
-      for (Rose_STL_Container<SgNode*>::iterator iter = loops.begin(); iter!= loops.end(); iter++) {
         SgNode *currentLoop = *iter;
         SgForStatement *fl = isSgForStatement(currentLoop);
-        cout << "\t\t Found a for loop at: " << fl->get_file_info()->get_line() << endl;
-        
-        analyzableLoops.insert(fl);
-      }// end for-loop for loops
 
-      vector<SgNode*> writeRefs;
-      vector<SgNode*> readRefs;
+        cout << "\t\t Found a for-loop to tile at: ";
+        cout << fl->get_file_info()->get_line() << endl;
 
-      SgStatement *fnDefnScope = SageInterface::getScope(defn);
-      SageInterface::collectReadWriteRefs(fnDefnScope, readRefs, writeRefs);
+        // Skip imperfectly nested loops and loops that are only singley nested
+        if (SageInterface::isCanonicalForLoop(fl)) {
+          //generateTiledProg(argc, argv, fileName, func->get_name().getString(),
+          //                  fl->get_mangled_name(), testcaseNum);
+          testcaseNum++;
+        } else {
+          cout << "\t\t\t Skipped single non-nested loop" << endl;
+        }
 
-      backend(project);
+      }// End for-loops loop
 
-    } // end for-loop for declarations
+    } // End declarations loop
 
-  } //end for-loop for files
+  } // End files loop
 
   cout << "Done ...\n";
-  
-  // Generate the source code
-  // return backend (project);
 }
-
--- GenerateTiledBenchmarks.C
+++ GenerateTiledBenchmarks.C
@@ -12,24 +12,64 @@
 
 using namespace std;
 
-void generateTiledProg(
-  int argc,
-  char *argv[],
-  SgForStatement *loopNest,
-  int loopDepth,
-  int counter
-) {
+int findNumberOfEnclosingLoops(SgNode* node) {
+  ROSE_ASSERT(node);
+
+  int numLoops = 0;
+  SgStatement* refStmt = SageInterface::getEnclosingStatement(node);
+  ROSE_ASSERT(refStmt);
+  SgForStatement* fl = isSgForStatement(
+    SageInterface::findEnclosingLoop(refStmt)
+  );
+
+  while (fl) {
+    numLoops++;
+    refStmt = SageInterface::getEnclosingStatement(fl->get_parent());
+    ROSE_ASSERT(refStmt);
+    fl = isSgForStatement(SageInterface::findEnclosingLoop(refStmt));
+  }
+
+  return numLoops;
+}
+
+void generateTiledProg(int argc, char *argv[], string fileName,
+                       string funcName, SgName loopName, int counter) {
+
   // Build a project
   SgProject *project = frontend(argc,argv);
   ROSE_ASSERT(project != NULL);
 
-  SageInterface::loopTiling(loopNest, loopDepth, 5);
+  // Get the function with our target loop
+  SgFunctionDeclaration *func = SageInterface::findFunctionDeclaration(
+      project, funcName, NULL, true);
+  ROSE_ASSERT(func);
+  SgFunctionDefinition *defn = func->get_definition();
+  ROSE_ASSERT(defn);
+  Rose_STL_Container<SgNode*> loops = NodeQuery::querySubTree(
+      defn,V_SgForStatement);
 
+  // Find the target loop and tile it
+  for (Rose_STL_Container<SgNode*>::iterator iter = loops.begin();
+       iter != loops.end(); iter++) {
+    SgNode *currentLoop = *iter;
+    SgForStatement *fl = isSgForStatement(currentLoop);
+    if (fl->get_mangled_name() == loopName) {
+      SageInterface::loopTiling(fl, 1, 66);
+      break;
+    }
+  }
+
+  // unparse tiled program
   backend(project);
 
   // Hacky solution to generate multiple tiled programs for test case
-  const string mvBinary = "mv a.out " + to_string(counter) + ".out";
-  const string mvSrc = "mv rose_symm.c rose_symm_" + to_string(counter) + ".c";
+  string baseName = fileName.substr(fileName.find_last_of("/\\") + 1);
+  string::size_type const extLoc(baseName.find_last_of('.'));
+  string baseNameNoExt = baseName.substr(0, extLoc);
+  const string mvBinary = "mv a.out " + baseNameNoExt +
+                          to_string(counter) + ".out";
+  const string mvSrc = "mv rose_" + baseName + " " + baseNameNoExt +
+                       to_string(counter) + ".c";
   system(mvBinary.c_str());
   system(mvSrc.c_str());
 }
@@ -42,67 +82,66 @@ int main(int argc, char *argv[]) {
 
   // For each source file in the project
   SgFilePtrList & ptr_list = project->get_fileList();
+  int testcaseNum = 0;
 
-  int counter = 0;
+  for (SgFilePtrList::iterator iter = ptr_list.begin(); iter != ptr_list.end();
+       iter++) {
 
-  for (SgFilePtrList::iterator iter = ptr_list.begin(); 
-               iter!=ptr_list.end(); iter++) { 
-    SgFile* sageFile = (*iter); 
-    SgSourceFile * sfile = isSgSourceFile(sageFile);
+    SgFile *sageFile = (*iter);
+    SgSourceFile *sfile = isSgSourceFile(sageFile);
     ROSE_ASSERT(sfile);
     SgGlobal *root = sfile->get_globalScope();
     SgDeclarationStatementPtrList& declList = root->get_declarations();
+    string fileName = sfile->getFileName();
 
-    cout << "Found a file: " << sfile->getFileName() << endl;
+    cout << "Found a file: " << fileName << endl;
 
-    //For each function body in the scope
-    for (SgDeclarationStatementPtrList::iterator p = declList.begin(); 
-            p != declList.end(); ++p) {
+    // For each function body in the scope
+    for (SgDeclarationStatementPtrList::iterator p = declList.begin();
+         p != declList.end(); ++p) {
+
       SgFunctionDeclaration *func = isSgFunctionDeclaration(*p);
       if (func == 0)  continue;
       SgFunctionDefinition *defn = func->get_definition();
       if (defn == 0)  continue;
-       //ignore functions in system headers, Can keep them to test robustness
-      if (defn->get_file_info()->get_filename()!=sageFile->get_file_info()->get_filename())
+      // Ignore functions in system headers, Can keep them to test robustness
+      if (defn->get_file_info()->get_filename()
+          != sageFile->get_file_info()->get_filename()) {
         continue;
+      }
 
       cout << "\t Found a function" << endl;
 
-      // Collect all analyable loops in this function
-      set<SgForStatement*> analyzableLoops;
+      // Collect all loops
+      Rose_STL_Container<SgNode*> loops = NodeQuery::querySubTree(
+          defn,V_SgForStatement);
+      if (loops.size() == 0) continue;
 
-      // For each loop 
-      Rose_STL_Container<SgNode*> loops = NodeQuery::querySubTree(defn,V_SgForStatement); 
-      if (loops.size()==0) continue;
+      // For each loop, tile the loop and unparse the program into a new file
+      for (Rose_STL_Container<SgNode*>::iterator iter = loops.begin();
+           iter != loops.end(); iter++) {
 
-      for (Rose_STL_Container<SgNode*>::iterator iter = loops.begin(); iter!= loops.end(); iter++) {
         SgNode *currentLoop = *iter;
         SgForStatement *fl = isSgForStatement(currentLoop);
-        cout << "\t\t Found a for loop at: " << fl->get_file_info()->get_line() << endl;
-        
-        analyzableLoops.insert(fl);
 
-//        generateTiledProg(argc, argv, fl, 1, counter);
-//        counter++;
+        cout << "\t\t Found a for-loop to tile at: ";
+        cout << fl->get_file_info()->get_line() << endl;
 
-        SageInterface::loopTiling(fl, 1, 10);
-        return backend(project);
+        // Skip imperfectly nested loops and loops that are only singley nested
+        if (SageInterface::isCanonicalForLoop(fl)
+            && findNumberOfEnclosingLoops(fl) > 1) {
+          generateTiledProg(argc, argv, fileName, func->get_name().getString(),
+                            fl->get_mangled_name(), testcaseNum);
+          testcaseNum++;
+        } else {
+          cout << "\t\t\t Skipped malformed and/or single non-nested loop" << endl;
+        }
 
-      }// end for-loop for loops
+      } // End for-loops loop
 
-      vector<SgNode*> writeRefs;
-      vector<SgNode*> readRefs;
+    } // End declarations loop
 
-      SgStatement *fnDefnScope = SageInterface::getScope(defn);
-      SageInterface::collectReadWriteRefs(fnDefnScope, readRefs, writeRefs);
-
-    } // end for-loop for declarations
-
-  } //end for-loop for files
+  } // End files loop
 
   cout << "Done ...\n";
-  
-  // Generate the source code
-  // return backend(project);
 }
-
